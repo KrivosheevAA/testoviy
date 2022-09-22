6e97565502f2b74b435657e712e2ff03f96577f1
Anton <krivosheev777@gmail.com>
2022-09-22 16:53:07 +0300

    первая практика

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log("ПРивет");
+'use strict';
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    gender: {},
+    privat: false
+};
+
+let queshen1 = prompt('Один из последних просмотренных фильмов?', "");
+let queshen2 = +prompt('На сколько цренете его?', "");
+let queshen3 = prompt('Один из последних просмотренных фильмов?', "");
+let queshen4 = +prompt('На сколько цренете его?', "");
+personalMovieDB.movies[queshen1] = queshen2;
+personalMovieDB.movies[queshen3] = queshen4;
+console.log(personalMovieDB);
